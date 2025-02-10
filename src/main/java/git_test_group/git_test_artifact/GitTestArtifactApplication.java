349228257d18bdb6e207d675d67c04f6b06c88f6
lwj0831 <wjl0831@gmail.com>
2025-02-10 16:26:58 +0900

    version1

---
+++ src/main/java/git_test_group/git_test_artifact/GitTestArtifactApplication.java
@@ -0,0 +1,13 @@
+package git_test_group.git_test_artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git_test_group/git_test_artifact/GitTestArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package git_test_group.git_test_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
